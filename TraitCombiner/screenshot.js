4c780d9bc7664e709128b173b44675477d6f2f1a
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-02-19 15:35:27 -0500

    updated with corn pop

--- TraitCombiner/screenshot.js
+++ TraitCombiner/screenshot.js
@@ -1,10 +1,14 @@
-const { default: html2canvas } = require("html2canvas");
-
-const canvas = await html2canvas(document.getElementById("Canvas"));
-
-const url = canvas.toDataURL("image/gif");
-
 // html2canvas(document.querySelector("#Canvas")).then(canvas => {
 //     document.body.appendChild(canvas)
 // });
+
+//attribute function
+
+async function createImage() {
+    const { default: html2canvas } = require("html2canvas");
+
+    const canvas = await html2canvas(document.getElementById("Canvas"));
+    
+    const url = canvas.toDataURL("image/gif");
+}
 
